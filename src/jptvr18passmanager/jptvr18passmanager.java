92fa754761f6330abe919c8bac85c610f591ddfe
pupil <pupil@ivkhk.ee>
2020-09-04 14:34:10 +0300

    точка входа

---
+++ src/jptvr18passmanager/jptvr18passmanager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr18passmanager;
+
+/**
+ *
+ * @author pupil
+ */
+public class jptvr18passmanager {
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
