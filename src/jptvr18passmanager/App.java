51d2ff8411a1507d931670f4062771d06f4a01f6
pupil <pupil@ivkhk.ee>
2020-09-04 15:19:07 +0300

    Начало реализации проэкта

--- src/jptvr18passmanager/App.java
+++ src/jptvr18passmanager/App.java
@@ -5,12 +5,51 @@
  */
 package jptvr18passmanager;
 
+import entity.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 class App {
+    private List<Resource> ListResources = new ArrayList<>();
     public void run(){
         System.out.println("LOH");
+        String repeat = "r";
+        do{
+        System.out.println("Выберите задачу: ");
+        System.out.println("0 - выйти из программы: ");
+        System.out.println("1 - добавить ресурс ");
+        System.out.println("2 - список ресурсов ");
+        
+        Scanner scanner = new Scanner(System.in);
+        int task = scanner.nextInt();
+        scanner.nextLine();
+        switch (task) {
+            case 0:
+                repeat = "q";
+                break;
+            case 1:
+                System.out.println("Выбрана задача 1");
+                Resource resource = new Resource();
+                resource.setName("ivkhk");
+                resource.setUrl("http://kutsehariduskeskus.ee");
+                resource.setLogin("admin");
+                resource.setPassword("123123");
+                ListResources.add(resource);
+                break;
+            case 2:
+                System.out.println("Выбрана задача 2");
+                System.out.println("Список ресурсов:");
+                for(Resource r : ListResources){
+                    System.out.println(r.toString());}
+                break;
+        }
+            }while(!"q".equals(repeat));
     }
+      
+     
 }
